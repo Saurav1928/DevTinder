25654135bb4f915424445d64e3ddae54ff507a56
Saurav Farkade <sauravfarkade9191@gmail.com>
2025-02-16 07:12:25 +0530

    exploring http methods and app.use

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,17 @@
-const express= require("express")
-const app= express();
-app.use("/", (req, res)=>res.send("Home page!!"))
-app.use("/test",(req, res)=> res.end("Hello from Test"));
-app.use("/user",(req, res)=> res.end("Hello from User"));
-app.listen(7000, console.log("App is running!!"));
+const express = require("express")
+const app = express()
+app.get("/user", (req, res)=>{
+    res.send({
+        firstName:"Saurav",
+        lastName:"Farkade"
+    })
+})
+app.post("/user",(req, res)=>{
+    res.send({
+        msg:"Data Saved successfully to DB!!"
+    })
+})
+app.use("/user", (req, res)=>{
+    res.send("Other than Get and Post requests!!!")
+})
+app.listen(7000, console.log("App is running at http://localhost:7000 !!"))

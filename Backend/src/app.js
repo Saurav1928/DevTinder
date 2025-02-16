2b7e29255531625c5969d41b13d28097cc71444a
Saurav Farkade <sauravfarkade9191@gmail.com>
2025-02-16 11:04:52 +0530

    handling multiple route handlers..

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,49 +1,30 @@
 const express = require("express")
 const app = express()
-// app.get("/user", (req, res)=>{
-//     res.send({
-//         firstName:"Saurav",
-//         lastName:"Farkade"
-//     })
-// })
-// app.post("/user",(req, res)=>{
-//     res.send({
-//         msg:"Data Saved successfully to DB!!"
-//     })
-// })
-// app.use("/user", (req, res)=>{
-//     res.send("Other than Get and Post requests!!!")
-// })
-
-// advance routing techniques!!
-// this will work for /abc
-// app.get("/abc", (req, res)=>{
-//     res.send("Hello!!")
-// })
-
-// this will work for /abc, /ac => as there is ? 
-// -> this makes b as optional
-// app.get("/ab?c", (req, res)=>{
-//     res.send("Hello!!")
-// })
-
-// this will work for /abc, /abbc, /abbbc, /ab........bc 
-// like + will make b for any cnt
-// app.get("/ab+c", (req, res)=>{
-//     res.send("Hello!!")
-// })
-
-// this will work for /abcd, /abdsbsdjfhgjdffcd anything in 
-// bw ab and cd but it should have ab at start and cd at end, pattern should match 
-// app.get("/ab*cd", (req, res)=>{
-//     res.send({msg:"Hello from server"})
-// })
-
-
-// to handle dynamic query params like id, passowrd, name etc
-app.get("/user/:userId/:password/:age", (req, res)=>{
-    console.log(req.params)
-    res.send("Received params:")
-})
-// regex can also be used in routing
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Hello from 1st req handler")
+    // res.send("1st response!!")
+    next()
+    console.log("end of 1st")
+  },
+ [ (req, res, next) => {
+    console.log("Hello from 2nd req handler")
+    // res.send("2nd response!!")
+    next()
+    console.log("end of 2nd")
+  },
+  (req, res, next) => {
+    console.log("Hello from 3rd req handler")
+    // res.send("3rd response!!")
+    next()
+    console.log("end of 3rd")
+  }],
+  (req, res, next) => {
+    console.log("Hello from 4th req handler")
+    res.send("4th response!!")
+    // next()
+    // console.log("end of 4th")
+  }
+)
 app.listen(7000, console.log("App is running at http://localhost:7000 !!"))

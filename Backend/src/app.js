37ec11f901930288109a76b25e2ac9fec3e23b42
Saurav Farkade <sauravfarkade9191@gmail.com>
2025-02-19 15:10:02 +0530

    updated the schema, played with mongoose options

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const { connectDB } = require("./config/database")
 const app = express()
-const User= require("./models/user.model")
+const User = require("./models/user.model")
 
-app.use(express.json())// without the path
-
-app.post("/signup",async (req, res, next)=>{
-  const newUser= req.body;
-  console.log(data)
+app.use(express.json()) // without the path
+//  get all users from DB with matching email
+app.post("/signup", async (req, res, next) => {
+  const data = req.body
+  // console.log(newUser)
   // const userObj={
   //   firstName :"Rohan",
   //   lastName:"Farkade",
@@ -16,19 +16,71 @@ app.post("/signup",async (req, res, next)=>{
   // }
   // // creating a new instance of User
   try {
-    const newUser= new User(data)
-  await newUser.save();
-  res.send(`User is saved to DB!!!`)
+    const existingUser = await User.findOne({ emailId: data.emailId });
+    if (existingUser) {
+      return res.status(400).send(`Email already exists. Please use another email.`);
+    }
+    const newUser = new User(data)
+    await newUser.save()
+    res.send(`User is saved to DB!!!`)
   } catch (error) {
-    res.status(500).send("Error Saving User!")
+    res.status(500).send(`Error Saving User! :  ${error.message}`)
   }
 })
 
+app.get("/userByEmail", async (req, res) => {
+  try {
+    const userEmail = req.body.emailId
+    const user = await User.find({ emailId: userEmail })
+    if (!user) {
+      res.status(404).send(`User with mail : ${userEmail} not found!!`)
+    } else {
+      res.send(user)
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong!!")
+  }
+})
+// feed api - get all users from DB
+app.get("/feed", async (req, res) => {
+  try {
+    const users= await User.find({});
+    if(!users){
+      res.status(404).send("Users not found!!")
+    }
+    else
+    res.send(users) 
 
-connectDB().then((result) => {
-  console.log("Successfully Connected to DB!!")
-  app.listen(7000, console.log("App is running at http://localhost:7000 !!"))
-}).catch((err) => {
-  console.log("Failed to connect to DB!!")
-});
+  } catch (error) {
+    res.status(400).send("Something went wrong!!")
+  }
+})
+app.patch("/user", async (req, res)=>{
+  const data = req.body;
+  const userId= data.userId;
+  try {
+    const updatedUser= await User.findByIdAndUpdate(userId, data, {runValidators:true});
+    res.send("User updated Successfully!")
+  } catch (error) {
+    res.status(400).send(`Error Updating User : ${error.message}`)
+  }
+})
 
+app.delete("/user", async (req, res)=>{
+  const userId= req.body.userId;
+  try {
+    const user= await User.findByIdAndDelete(userId);
+    res.send("User Deleted Successfully!!");
+  } catch (error) {
+ res.send("Error while deleting a user!!")   
+  }
+})
+
+connectDB()
+  .then((result) => {
+    console.log("Successfully Connected to DB!!")
+    app.listen(7000, console.log("App is running at http://localhost:7000 !!"))
+  })
+  .catch((err) => {
+    console.log("Failed to connect to DB!!")
+  })
--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,24 +1,48 @@
 const mongoose = require("mongoose")
-const { Schema } = mongoose;
-const userSchema = new Schema({
+const { Schema } = mongoose
+const userSchema = new Schema(
+  {
   firstName: {
     type: String,
+    required: true,
+    minLength:4,
+    maxLength:50
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
   },
   password: {
     type: String,
+    required: true,
   },
   age: {
     type: Number,
+    min:18,
+    max:110
   },
   gender: {
     type: String,
+    validate(value){
+      if(!(["male", "female", "other"].includes(value))) throw new Error("Gender is invalid!!")
+    }
   },
-})
+  photoUrl: {
+    type: String,
+  },
+  skills: {
+    type: [String],
+  },
+  about: {
+    type: String,
+    default: "This is default about!!",
+  },
+},{timestamps:true})
 
 module.exports = mongoose.model("User", userSchema)

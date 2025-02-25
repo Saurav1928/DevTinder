a1d92016accf0e130c5ad0d2afc3ecf1c088af71
Saurav Farkade <sauravfarkade9191@gmail.com>
2025-02-25 07:05:37 +0530

    feed api created

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -2,7 +2,8 @@ const express = require("express")
 const { userAuth } = require("../middlewares/auth")
 const ConnectionRequest = require("../models/connectionRequest.model")
 const userRouter = express.Router()
-const PUBLIC_SAFE_DATA=["firstName", "lastName", "photoUrl", "about", "skills"];
+const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl", "about", "skills"]
+const User = require("../models/user.model")
 // get all the received pending interested requests of loggedIn user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -10,7 +11,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", PUBLIC_SAFE_DATA)
+    }).populate("fromUserId", USER_SAFE_DATA)
     if (connectionRequests.length == 0)
       res.json({ message: "No Request Found.." })
     else res.json({ message: "Requests found", connectionRequests })
@@ -20,26 +21,55 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
 })
 
 // /user/requests/accepted -> to get all the status = accepted connections..
-userRouter.get("/user/requests/accepted", userAuth, async (req, res)=>{
-    try {
-        const loggedInUser=req.user
-        const acceptedConnections=await ConnectionRequest.find({
-            $or:[
-                {toUserId:loggedInUser._id},
-                {fromUserId:loggedInUser._id}
-            ],
-            status:"accepted"
-        }).populate("fromUserId", PUBLIC_SAFE_DATA).populate("toUserId", PUBLIC_SAFE_DATA)
-        const data= acceptedConnections.map((row)=>{
-            if(row.fromUserId._id.toString()===loggedInUser._id.toString())
-                return row.toUserId;
-            return row.fromUserId;
-        })
-        res.json({
-            message :"All Accepted Connections Found...", data
-        })
-    } catch (error) {
-        res.json({ message: "Error : ", error })
-      }
+userRouter.get("/user/requests/accepted", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user
+    const acceptedConnections = await ConnectionRequest.find({
+      $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
+      status: "accepted",
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA)
+    const data = acceptedConnections.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString())
+        return row.toUserId
+      return row.fromUserId
+    })
+    res.json({
+      message: "All Accepted Connections Found...",
+      data,
+    })
+  } catch (error) {
+    res.json({ message: "Error : ", error })
+  }
+})
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    /*
+       logged in user should be able to see all the users except
+      - himself
+      - users who has already sent a request to loggedInUser (interseted, rejected, accepted, ignored) or loggedInUser has sent a connection request to user
+      ----------------------------------------------------- APPROACH ----------------------------------------------------------
+      - loop over all the connections and add all the toUser and fromUser where (toUser=loggedInUser or fromUser=loggedInUser)
+      - use set to store all the connections id to avoid duplicate
+
+    */
+    const loggedInUser = req.user
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    })
+    const usersWhomLoggedInUserWantToHide = new Set()
+    connectionRequests.forEach((req) => {
+      usersWhomLoggedInUserWantToHide.add(req.fromUserId.toString())
+      usersWhomLoggedInUserWantToHide.add(req.toUserId.toString())
+    })
+    const feed = await User.find({
+      _id: { $nin: Array.from(usersWhomLoggedInUserWantToHide) },
+    }).select(USER_SAFE_DATA)
+    res.json(feed)
+  } catch (error) {
+    res.status(400).send("ERROR:" + error)
+  }
 })
 module.exports = { userRouter }
